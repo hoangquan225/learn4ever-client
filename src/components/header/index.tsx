bce674d348c83322626f30b0d2e42d347f79ac39
DangThaiSon <changlinhgo5@gmail.com>
2022-12-31 14:12:31 +0700

    them navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,15 +10,20 @@ import logo from "../../assets/img/logo.png";
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
 import { RootState } from "../../redux/store";
 import { Dropdown, MenuProps, notification } from "antd";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { AiOutlineUser } from "react-icons/ai";
 import { requestGetUserFromToken } from "../../redux/slices/authSlice";
 import { unwrapResult } from "@reduxjs/toolkit";
+import { FaBars } from "react-icons/fa";
+import { GrClose } from "react-icons/gr";
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const [navbarStick, setNavbarStick] = useState(false);
+
   const userInfo = useAppSelector(
     (state: RootState) => state.authState.userInfo
   );
@@ -78,6 +83,34 @@ const Header = () => {
       },
     },
   ];
+
+  const handleNavbar = () => {
+    if (showNavbar === true) {
+      document.body.style.overflowY = "scroll";
+      setShowNavbar(!showNavbar);
+    } else {
+      document.body.style.overflowY = "hidden";
+      setShowNavbar(!showNavbar);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleNavbarStick);
+
+    return () => {
+      window.removeEventListener("scroll", handleNavbarStick);
+    };
+  }, []);
+
+  const handleNavbarStick = () => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      windowHeight > 180
+        ? setNavbarStick(!navbarStick)
+        : setNavbarStick(navbarStick);
+    }
+  };
+
   return (
     <>
       <header className={cx("header")}>
@@ -117,6 +150,212 @@ const Header = () => {
           </div>
         </div>
       </header>
+
+      <nav
+        className={navbarStick ? cx("navbar", "stick") : cx("navbar")}
+        onScroll={handleNavbarStick}
+      >
+        <div className={cx("wide")}>
+          <div className={cx("navbar__container")}>
+            <div className={cx("navbar__list")}>
+              {/* DESKTOP */}
+              <div className={cx("navbar__list--desktop")}>
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 1</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 2</span>
+                    </div>
+                  </Link>
+                </div>
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 3</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 4</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 5</span>
+                    </div>
+                  </Link>
+                </div>
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 6</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 7</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 8</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 9</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 10</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 11</span>
+                    </div>
+                  </Link>
+                </div>
+
+                <div className={cx("navbar__item--desktop")}>
+                  <Link to={"/"} className={cx("navbar__link--desktop")}>
+                    <div className={cx("navbar__title")}>
+                      <span>Lớp 12</span>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+
+              {/* MOBILE */}
+              <div className={cx("navbar__icon--open")}>
+                <button className={cx("navbar__btn")} onClick={handleNavbar}>
+                  <FaBars className={cx("nav__icon")} />
+                </button>
+              </div>
+
+              <div className={cx("navbar__sidebar")}>
+                <div
+                  className={
+                    showNavbar ? cx("overlay", "active") : cx("overlay")
+                  }
+                  onClick={handleNavbar}
+                ></div>
+                <div
+                  className={
+                    showNavbar
+                      ? cx("navbar__list--mobile", "active")
+                      : cx("navbar__list--mobile")
+                  }
+                >
+                  <div className={cx("navbar__icon--close")}>
+                    <button
+                      className={cx("navbar__btn")}
+                      onClick={handleNavbar}
+                    >
+                      <GrClose className={cx("nav__icon")} />
+                    </button>
+                  </div>
+
+                  <div className={cx("navbar__item--mobile")}>
+                    <Link to={"/"} className={cx("navbar__link--mobile")}>
+                      <div className={cx("navbar__title")}>
+                        <span>Luyện Thi THPT QG</span>
+                      </div>
+                    </Link>
+                  </div>
+
+                  <div className={cx("navbar__item--mobile")}>
+                    <Link to={"/"} className={cx("navbar__link--mobile")}>
+                      <div className={cx("navbar__title")}>
+                        <span>Luyện Thi THPT QG</span>
+                      </div>
+                    </Link>
+                  </div>
+
+                  <div className={cx("navbar__item--mobile")}>
+                    <Link to={"/"} className={cx("navbar__link--mobile")}>
+                      <div className={cx("navbar__title")}>
+                        <span>Luyện Thi THPT QG</span>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <Link
+              to={"/"}
+              className={
+                navbarStick
+                  ? cx("header__link", "link__nav")
+                  : cx("header__link", "link__nav", "hide")
+              }
+              onScroll={handleNavbarStick}
+            >
+              <div className={cx("header__logo")}>
+                <img src={logo} alt="logo" className={cx("header__img")} />
+              </div>
+            </Link>
+
+            <div
+              className={
+                navbarStick ? cx("header__auth") : cx("header__auth", "hide")
+              }
+              onScroll={handleNavbarStick}
+            >
+              {!userInfo?._id ? (
+                <>
+                  <Link to={"/login"} className={cx("header__btn--link")}>
+                    <button className={cx("header__button")}>Đăng nhập</button>
+                  </Link>
+                  <Link to={"/register"} className={cx("header__btn--link")}>
+                    <button className={cx("header__button")}>Đăng ký</button>
+                  </Link>
+                </>
+              ) : (
+                <Dropdown
+                  menu={{ items }}
+                  trigger={["hover"]}
+                  placement={"bottomRight"}
+                >
+                  <button className={cx("header__button")}>
+                    <AiOutlineUser />
+                  </button>
+                </Dropdown>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
