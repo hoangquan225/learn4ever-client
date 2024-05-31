439b46fbcfb0be4c09bea6d237269dba5d5e2511
quanhh <quanhh@vnpt.vn>
2024-05-31 12:37:09 +0700

    update

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,40 +1,40 @@
 export const classes = [
-  {
-    label: "Lớp 1",
-    value: 1,
-  },
-  {
-    label: "Lớp 2",
-    value: 2,
-  },
-  {
-    label: "Lớp 3",
-    value: 3,
-  },
-  {
-    label: "Lớp 4",
-    value: 4,
-  },
-  {
-    label: "Lớp 5",
-    value: 5,
-  },
-  {
-    label: "Lớp 6",
-    value: 6,
-  },
-  {
-    label: "Lớp 7",
-    value: 7,
-  },
-  {
-    label: "Lớp 8",
-    value: 8,
-  },
-  {
-    label: "Lớp 9",
-    value: 9,
-  },
+  // {
+  //   label: "Lớp 1",
+  //   value: 1,
+  // },
+  // {
+  //   label: "Lớp 2",
+  //   value: 2,
+  // },
+  // {
+  //   label: "Lớp 3",
+  //   value: 3,
+  // },
+  // {
+  //   label: "Lớp 4",
+  //   value: 4,
+  // },
+  // {
+  //   label: "Lớp 5",
+  //   value: 5,
+  // },
+  // {
+  //   label: "Lớp 6",
+  //   value: 6,
+  // },
+  // {
+  //   label: "Lớp 7",
+  //   value: 7,
+  // },
+  // {
+  //   label: "Lớp 8",
+  //   value: 8,
+  // },
+  // {
+  //   label: "Lớp 9",
+  //   value: 9,
+  // },
   {
     label: "Lớp 10",
     value: 10,
@@ -52,15 +52,15 @@ export const classes = [
 export const genders = [
   {
     label: "Nam",
-    value: 0,
-  },
-  {
-    label: "Nữ",
     value: 1,
   },
   {
-    label: "Khác",
+    label: "Nữ",
     value: 2,
+  },
+  {
+    label: "Khác",
+    value: 3,
   },
 ];
 
